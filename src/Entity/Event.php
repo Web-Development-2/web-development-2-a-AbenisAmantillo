6b59d601417b7c7fc2e183661deb98db7c74719d
Abenis Amantillo Jr <analyncuevas8@gmail.com>
2026-02-20 20:21:15 +0800

    handson_A

---
+++ src/Entity/Event.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EventRepository::class)]
+class Event
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?\DateTime $startDate = null;
+
+    #[ORM\Column]
+    private ?\DateTime $endDate = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?int $maxAttendees = null;
+
+    #[ORM\Column]
+    private ?bool $status = null;
+
+    #[ORM\ManyToOne(inversedBy: 'events')]
+    private ?Venue $Venue = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTime
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTime $startDate): static
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTime
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTime $endDate): static
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getMaxAttendees(): ?int
+    {
+        return $this->maxAttendees;
+    }
+
+    public function setMaxAttendees(int $maxAttendees): static
+    {
+        $this->maxAttendees = $maxAttendees;
+
+        return $this;
+    }
+
+    public function isStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getVenue(): ?Venue
+    {
+        return $this->Venue;
+    }
+
+    public function setVenue(?Venue $Venue): static
+    {
+        $this->Venue = $Venue;
+
+        return $this;
+    }
+}
